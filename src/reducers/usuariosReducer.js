8f187d7513aa30159d68854d5ea562baacfb8b41
JoshuaMedinaCorona <JoshuaMedinaCorona@users.noreply.github.com>
2018-11-13 16:24:12 -0600

    .

--- src/reducers/usuariosReducer.js
+++ src/reducers/usuariosReducer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { LLAMAR, EXITOSO, FALLO, EDITNAME } from '../types/usuariosTypes';
 
 const INITIAL_STATE = {
@@ -18,25 +17,4 @@ export default (state = INITIAL_STATE, action) =>
 		case EDITNAME: return { ...state, nombre: action.payload };
 		default: return state;
 	}
-=======
-import { LLAMAR, EXITOSO, FALLO, EDITNAME } from '../types/usuariosTypes';
-
-const INITIAL_STATE = {
-	usuarios: [],
-	cargando: false,
-	error: '',
-	nombre: ''
-};
-
-export default (state = INITIAL_STATE, action) =>
-{
-	switch (action.type)
-	{
-		case LLAMAR: return { ...state, error: '', cargando: true };
-		case EXITOSO: return { ...state, error: '', cargando: false, usuarios: action.payload };
-		case FALLO: return { ...state, error: action.payload, cargando: false };
-		case EDITNAME: return { ...state, nombre: action.payload };
-		default: return state;
-	}
->>>>>>> 7c1d0f17a8b753048d80c3ab8536f6879f391dca
 }
--- src/types/usuariosTypes.js
+++ src/types/usuariosTypes.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 export const LLAMAR = 'usuarios_llamar';
 export const EXITOSO = 'usuarios_exitoso';
 export const FALLO = 'usuarios_fallo';
 export const EDITNAME = 'cambio_nombre';   
 
-=======
-export const LLAMAR = 'usuarios_llamar';
-export const EXITOSO = 'usuarios_exitoso';
-export const FALLO = 'usuarios_fallo';
-export const EDITANAME = 'cambio_nombre';   
-
->>>>>>> 7c1d0f17a8b753048d80c3ab8536f6879f391dca

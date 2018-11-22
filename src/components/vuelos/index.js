d605b9e8c092926b5851e03cf60272deaf74b861
Martin Pereyra <pereyra.gamboa@outlook.com>
2018-11-21 21:10:38 -0600

    Componente DetalleVuelo: contiene el formulario del modal original de Vuelos

--- src/components/vuelos/index.js
+++ src/components/vuelos/index.js
@@ -58,11 +58,10 @@ import * as vuelosActions from '../../actions/vuelosActions';
   render() {
       return (
           <div>
-            <Row>
               <h3 className="valign-wrapper left">Vuelos</h3>
                 <Modal
                     header='Agregue la informacion necesaria'
-                    trigger={<Button floating large className='green lighten-1 right' waves='light' icon='flight_takeoff'/>}>
+                    trigger={<Button floating large className='green lighten-1 valign-wrapper right' waves='light' icon='flight_takeoff'/>}>
                         <Row>
                             <Input  s={6} label="Origen" />
                             <Input  s={6} label="Destino" />
@@ -70,13 +69,11 @@ import * as vuelosActions from '../../actions/vuelosActions';
                             <Input  label="Llegada" s={6} />
                             <Input  label="IDRuta" s={6} />
                             <Input  label="Matricula" s={6} />
-                            <Input  label="Manifiesto" s={6} />
                         </Row>
                 </Modal>
-                {
-                    (this.props.cargando) ? this.desplegarCargando() : this.desplegarContenido()
-                }
-            </Row>
+              {
+                  (this.props.cargando) ? this.desplegarCargando() : this.desplegarContenido()
+              }
           </div>
       );
     }

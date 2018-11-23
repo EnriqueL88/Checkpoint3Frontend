db84784c7336fd8b3e47affe54b445180af70c39
JoshuaMedinaCorona <JoshuaMedinaCorona@users.noreply.github.com>
2018-11-23 16:37:00 -0600

    link color

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,19 +17,19 @@ const Body = (props) => (
 		<div className="container">
 			<Collection>
 				<CollectionItem>
-          <Link className="purple-text text-darken-2" to="/usuarios">
+          <Link className="blue-text text-darken-2" to="/usuarios">
 						<h4><i className="material-icons">perm_contact_calendar</i> Usuarios</h4>
 					</Link>
 					<p>Consulte si necesita detalles de los pasajeros.</p>
 				</CollectionItem>
 				<CollectionItem>
-					<Link className="purple-text text-darken-2"  to="/vuelos">
+					<Link className="blue-text text-darken-2"  to="/vuelos">
 						<h4><i className="material-icons">flight_takeoff</i> Vuelos</h4>
 					</Link>
 					<p>Consulte si requiere revisar vuelos y su estatus.</p>
 				</CollectionItem>
 				<CollectionItem>
-					<Link className="purple-text text-darken-2" to="/aerolineas">
+					<Link className="blue-text text-darken-2" to="/aerolineas">
 						<h4><i className="material-icons">business</i> Aerolineas</h4>
 					</Link>
 					<p>Consulte si requiere saber sobre las aerolíneas.</p>

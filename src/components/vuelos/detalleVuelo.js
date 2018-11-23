a2b5ad7cf2dae82eb96f82349ec356a9061a0e35
Martin Pereyra <pereyra.gamboa@outlook.com>
2018-11-23 03:49:04 -0600

    DetalleVuelo modifica vuelos existentes

--- src/components/vuelos/detalleVuelo.js
+++ src/components/vuelos/detalleVuelo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import * as Datetime from 'react-datetime';
-import { Button, Col, Input, Row } from 'react-materialize';
+import { Button, Col, Input, Preloader, Row } from 'react-materialize';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import * as VuelosActions from '../../actions/vuelosActions';
@@ -71,12 +71,15 @@ class DetalleVuelo extends Component {
         origen, destino, ruta, idEstado
       } = this.props;
       const vuelo = {
-        IDVuelo, matriculaID, fechaSalida, fechaLlegada,
+        IDVuelo, matriculaID,
+        fechaSalida: new Date(fechaSalida).toJSON(),
+        fechaLlegada: new Date(fechaLlegada).toJSON(),
         origen, destino, ruta, idEstado
       };
       if (this.props.match.params.id) {
         // Guarda un vuelo preexistente
         this.props.modificarVuelo(vuelo);
+        //alert(vuelo.fechaSalida);
       }
       else {
         // Guarda un vuelo nuevo
@@ -85,66 +88,79 @@ class DetalleVuelo extends Component {
     }
   };
 
+  mostrarError = () => {
+    console.log(this.props.error);
+    return (<h3 className="red-text">{ this.props.error }</h3>);
+  };
+
+  mostrarFormulario = () => (
+    <div>
+      <Row>
+        <Input label="Identificador de ruta" s={6} m={4} offset="m4"
+               value={this.props.ruta} onChange={this.cambiarRuta}/>
+        <Input type="select" label="Estado" s={6} m={4}
+               value={this.props.idEstado} onChange={this.cambiarEstado}>
+          <option key="select" value={0}>(seleccione estado)</option>
+          {Object.entries(this.props.estados).map(estado => {
+            return (
+              <option key={estado[0]} value={estado[0]}>{estado[1]}</option>
+            )})}
+        </Input>
+      </Row>
+      <h5>Avión</h5>
+      {
+        this.props.matriculaID === 0 ?
+        (<Row>
+          <Input s={6} m={4} type="select" label="Aerolínea" onChange={this.mostrarListaMatriculas}>
+            <option key="select" value={0}>(seleccione aerolínea)</option>
+            {this.props.aerolineas.map(aerolinea => (
+              <option key={aerolinea.IDAerolinea} value={aerolinea.IDAerolinea}>{aerolinea.nombre}</option>
+            ))}
+          </Input>
+          <Input s={6} m={4} type="select" label="Avión"
+                 value={this.props.matriculaID} onChange={this.cambiarMatricula}>
+            <option key={0} value={0}>(seleccione matrícula)</option>
+            {this.props.matriculas.map(matricula => (
+              <option key={matricula} value={matricula}>{matricula}</option>
+            ))}
+          </Input>
+        </Row>) :
+        (<Row>
+          <Col s={6} m={4}>Matrícula: <b>{this.props.matriculaID}</b></Col>
+          <Button className="col s6 m4" onClick={() => this.props.cambiarMatricula(0)}>Cambiar</Button>
+        </Row>)
+      }
+      <h5>Detalles</h5>
+      <Row>
+        <Input s={4} label="Origen" value={this.props.origen} onChange={this.cambiarOrigen} />
+        <div className="col s8">
+          <span>Fecha de salida</span>
+          <Datetime value={this.props.fechaSalida} onChange={this.cambiarFechaSalida} />
+        </div>
+        <Input s={4} label="Destino" value={this.props.destino} onChange={this.cambiarDestino} />
+        <div className="col s8">
+          <span>Fecha de llegada</span>
+          <Datetime value={this.props.fechaLlegada} onChange={this.cambiarFechaLlegada} />
+        </div>
+      </Row>
+      <Row>
+        <Button className="purple col s6 m3 offset-m6" onClick={this.guardarVuelo}>Guardar</Button>
+        <Link to="/vuelos">
+          <Button className="green darken-2 col s6 m3">Cancelar</Button>
+        </Link>
+      </Row>
+    </div>);
+
+
+mostrarContenido = () => this.props.error ? this.mostrarError() : this.mostrarFormulario();
+
+  mostrarCargando = () => (<div className="center"><Preloader size="big" /></div>)
+
   render() {
     return (
       <div>
         <h3>Agregar nuevo vuelo</h3>
-        <Row>
-          <Input label="Identificador de ruta" s={6} m={4} offset="m4"
-                 value={this.props.ruta} onChange={this.cambiarRuta}/>
-          <Input type="select" label="Estado" s={6} m={4}
-                 value={this.props.idEstado} onChange={this.cambiarEstado}>
-            <option key="select" value={0}>(seleccione estado)</option>
-            {Object.entries(this.props.estados).map(estado => {
-              console.log(estado);
-              return (
-              <option key={estado[0]} value={estado[0]}>{estado[1]}</option>
-            )})}
-          </Input>
-        </Row>
-        <h5>Avión</h5>
-        {
-          this.props.matriculaID === 0 ?
-          (<Row>
-            <Input s={6} m={4} type="select" label="Aerolínea" onChange={this.mostrarListaMatriculas}>
-              <option key="select" value={0}>(seleccione aerolínea)</option>
-              {this.props.aerolineas.map(aerolinea => (
-                <option key={aerolinea.IDAerolinea} value={aerolinea.IDAerolinea}>{aerolinea.nombre}</option>
-              ))
-              }
-            </Input>
-            <Input s={6} m={4} type="select" label="Avión"
-                   value={this.props.matriculaID} onChange={this.cambiarMatricula}>
-              <option key={0} value={0}>(seleccione matrícula)</option>
-              {this.props.matriculas.map(matricula => (
-                <option key={matricula} value={matricula}>{matricula}</option>
-              ))}
-            </Input>
-          </Row>) :
-          (<Row>
-            <Col s={6} m={4}>Matrícula: <b>{this.props.matriculaID}</b></Col>
-            <Button className="col s6 m4" onClick={() => this.props.cambiarMatricula(0)}>Cambiar</Button>
-          </Row>)
-        }
-        <h5>Detalles</h5>
-        <Row>
-          <Input s={4} label="Origen" value={this.props.origen} onChange={this.cambiarOrigen} />
-          <div className="col s8">
-            <span>Fecha de salida</span>
-            <Datetime value={this.props.fechaSalida} onChange={this.cambiarFechaSalida} />
-          </div>
-          <Input s={4} label="Destino" value={this.props.destino} onChange={this.cambiarDestino} />
-          <div className="col s8">
-            <span>Fecha de llegada</span>
-            <Datetime value={this.props.fechaLlegada} onChange={this.cambiarFechaLlegada} />
-          </div>
-        </Row>
-        <Row>
-          <Button className="purple col s6 m3 offset-m6" onClick={this.guardarVuelo}>Guardar</Button>
-          <Link to="/vuelos">
-            <Button className="green darken-2 col s6 m3">Cancelar</Button>
-          </Link>
-        </Row>
+        { this.props.cargando ? this.mostrarCargando() : this.mostrarContenido() }
       </div>
     );
   }
